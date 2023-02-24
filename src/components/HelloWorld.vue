fa48a7df9f370939f644de3fca55e2913204d2f7
Devin <xiangqunXiao@poper.co>
2023-02-24 09:55:27 +0000

    feat: add element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <el-button type="primary" @click="count++">count is {{ count }}</el-button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -25,7 +25,7 @@ const count = ref(0)
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <el-link type="primary" href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</el-link>
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
